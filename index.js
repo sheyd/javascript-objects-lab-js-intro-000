f3df7c5528e37ed9a3e4f7522051e12685e68ef9
Sena Heydari <sheydari@dosomething.org>
2020-05-07 03:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var copyObj = Object.assign({}, object);
+  return delete newObj.key;
+}
